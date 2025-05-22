b912f3ebd99d705615d6e689134cf1cb71cae9e5
Denys <pedan.denys@gmail.com>
2025-05-22 20:24:06 +1200

    SoilMoistureItem
    Added circle indicator of soil moister

--- app/src/main/java/denys/diomaxius/aws/ui/screen/home/SoilMoistureItem.kt
+++ app/src/main/java/denys/diomaxius/aws/ui/screen/home/SoilMoistureItem.kt
@@ -1,11 +1,16 @@
 package denys.diomaxius.aws.ui.screen.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -13,8 +18,10 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,12 +47,27 @@ fun SoilMoistureItem(item: SoilMoisture) {
                 .padding(8.dp)
                 .clip(RoundedCornerShape(32.dp)),
         ) {
-            Text(
-                modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
-                text = item.deviceId,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.SemiBold
-            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp, vertical = 4.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    modifier = Modifier,
+                    text = item.deviceId,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                Box(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(16.dp)
+                        .background(circleColor(item.soilmoisture.toInt()))
+                )
+            }
 
             HorizontalDivider(
                 thickness = 2.dp,
@@ -89,6 +111,13 @@ fun SoilMoistureItem(item: SoilMoisture) {
     }
 }
 
+fun circleColor(soilMoister: Int) : Color {
+    return when {
+        soilMoister < 120 -> Color.Red
+        soilMoister in 120..180 -> Color.Yellow
+        else -> Color.Green
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
@@ -97,7 +126,7 @@ fun SoilMoistureItemPreview() {
         item = SoilMoisture(
             deviceId = "Example_Device_Id",
             timestamp = 1234567890,
-            soilmoisture = "Example_Soil_Moisture_Value"
+            soilmoisture = "160"
         )
     )
 }
